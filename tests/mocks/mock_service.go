e568a7c2d74cb7b97f5e1dbba7ee7d43756ff908
gamassss <gamassakbar@gmail.com>
2025-12-13 21:11:23 +0700

    test: add mock objects for testing

---
+++ tests/mocks/mock_service.go
@@ -0,0 +1,33 @@
+package mocks
+
+import (
+	"context"
+
+	"github.com/gamassss/url-shortener/internal/domain"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockShortenerService struct {
+	mock.Mock
+}
+
+var _ interface {
+	ShortenURL(ctx context.Context, req *domain.CreatedURLRequest) (*domain.URL, error)
+	GetOriginalURL(ctx context.Context, shortCode string) (*domain.URL, error)
+} = (*MockShortenerService)(nil)
+
+func (m *MockShortenerService) ShortenURL(ctx context.Context, req *domain.CreatedURLRequest) (*domain.URL, error) {
+	args := m.Called(ctx, req)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*domain.URL), args.Error(1)
+}
+
+func (m *MockShortenerService) GetOriginalURL(ctx context.Context, shortCode string) (*domain.URL, error) {
+	args := m.Called(ctx, shortCode)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*domain.URL), args.Error(1)
+}
